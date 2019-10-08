c3805b6ba28feba4c2caf8ec93ff53b317c1dfac
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-08 12:53:05 +0000

    Updated router.php. This whole file is a bit temporary, I think.

--- router.php
+++ router.php
@@ -3,4 +3,25 @@
 require_once('vendor/autoload.php');
 session_start();
 
-require_once('home.php');
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case '/':
+        $file = 'home.php';
+        break;
+
+    case '/login':
+        $file = 'login.php';
+        break;
+
+    case '/register':
+        $file = 'register.php';
+        break;
+
+    default:
+        $file = '404.php';
+        break;
+}
+
+require __DIR__ . '/' . $file;
+
+#require_once('home.php');
