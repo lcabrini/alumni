38d05ef664ed0155fc2276d6f6b364b379f25192
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-10 10:30:50 +0000

    Initial schema.

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+connect alumni
+
+create table if not exists `users` (
+    `user_id` int(11) auto_increment,
+    `email` varchar(100) not null,
+    `password` varchar(255) not null,
+    primary key(user_id),
+    unique(email)
+) engine=InnoDB default charset=utf8;
+
+insert into `users`(email, password) values('admin', password('s3kr3t'));
+
+
