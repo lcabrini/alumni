a2cb2486766fe25d5d617acbef0377d8dd47efc5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-11 17:12:29 +0000

    Added field for year graduated.

--- database/schema.sql
+++ database/schema.sql
@@ -4,10 +4,12 @@ create table if not exists `users` (
     `user_id` int(11) auto_increment,
     `email` varchar(100) not null,
     `password` varchar(255) not null,
+    `year_graduated` smallint not null,
     primary key(user_id),
     unique(email)
 ) engine=InnoDB default charset=utf8;
 
-insert into `users`(email, password) values('admin@example.com', password('s3kr3t'));
+insert into `users`(email, password, year_graduated) values('admin@example.com', password('s3kr3t'), 2000);
+
 
 
