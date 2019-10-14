b8caa4e121f4cf2b823981ab28a98ce374c16ac9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-14 19:11:40 +0000

    Save new user when completing registration form.

--- alumni/routes/register.php
+++ alumni/routes/register.php
@@ -1,11 +1,15 @@
 <?php
 
 require 'util.php';
+require 'users.php';
 
 $twig = get_twig();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "post";
+    $user['email'] = $_POST['email'];
+    $user['password'] = $_POST['password'];
+    $user['year_graduated'] = $_POST['year_graduated'];
+    add_user($user);
 } else {
     echo $twig->render('register_form.twig');
 }
