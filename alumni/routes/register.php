cd08b114394b40729bacb17c0f1892cf5c5c3be9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-15 17:36:59 +0000

    Display error if user trying to register with email that is already registered.

--- alumni/routes/register.php
+++ alumni/routes/register.php
@@ -6,11 +6,16 @@ require 'users.php';
 $twig = get_twig();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user['email'] = $_POST['email'];
-    $user['password'] = $_POST['password'];
-    $user['year_graduated'] = $_POST['year_graduated'];
-    add_user($user);
-    sendmail($user['email'], "This is a test message"); 
+    if (!email_exists($_POST['email'])) {
+        echo $twig->render('user_exists.twig', 
+            ['email' => $_POST['email']]);
+    } else {
+        $user['email'] = $_POST['email'];
+        $user['password'] = $_POST['password'];
+        $user['year_graduated'] = $_POST['year_graduated'];
+        add_user($user);
+        sendmail($user['email'], "This is a test message");
+    }
 } else {
     echo $twig->render('register_form.twig');
 }
