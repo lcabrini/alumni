46f75379596315f1033756a03ba9709e6ad545cc
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-15 17:14:16 +0000

    Send an email to user upon registration.

--- alumni/routes/register.php
+++ alumni/routes/register.php
@@ -10,6 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user['password'] = $_POST['password'];
     $user['year_graduated'] = $_POST['year_graduated'];
     add_user($user);
+    sendmail($user['email'], "This is a test message"); 
 } else {
     echo $twig->render('register_form.twig');
 }
