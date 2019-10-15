6a070bad5289debb47d8c2654aab13ffe31f4d08
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-15 17:50:15 +0000

    Check that passwords match during registration.

--- alumni/routes/register.php
+++ alumni/routes/register.php
@@ -9,6 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!email_exists($_POST['email'])) {
         echo $twig->render('user_exists.twig', 
             ['email' => $_POST['email']]);
+    } elseif ($_POST['password'] != $_POST['password2']) {
+        $_SESSION['message'] = array(
+            'type' => 'is-danger',
+            'text' => "The passwords don't match"
+        );
+        header("Location: /register");
     } else {
         $user['email'] = $_POST['email'];
         $user['password'] = $_POST['password'];
