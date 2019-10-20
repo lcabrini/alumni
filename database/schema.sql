856ff24e122810a5ff0f9f8de32d93be58091417
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-20 15:44:30 +0000

    Update database schema. Refs #7

--- database/schema.sql
+++ database/schema.sql
@@ -26,3 +26,19 @@ create table if not exists confirmation_codes (
 create trigger confirmation_code_create before insert on confirmation_codes
 for each row set
     new.expires = timestampadd(day, 1, now());
+
+create table if not exists content(
+    content_id int(11) auto_increment,
+    content_key varchar(20) not null,
+    title varchar(255) not null,
+    subtitle varchar(255) not null default '',
+    body text not null,
+    primary key(content_id)
+) engine=InnoDB default charset=utf8;
+
+insert into content(content_key, title, subtitle, body) values(
+    'about_us',
+    'About Us',
+    '',
+    '<p>Todo: write content here.</p>'
+);
