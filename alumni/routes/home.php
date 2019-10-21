bc9e7c03de45d85e3d91445641dfa8f02e4728e7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-21 11:28:39 +0000

    Fetch intro message. Refs #11

--- alumni/routes/home.php
+++ alumni/routes/home.php
@@ -3,4 +3,11 @@
 require_once('util.php');
 
 $twig = get_twig();
-echo $twig->render('home.twig', ['testvar' => 'a little test']);
+$db = get_database();
+$sql = "SELECT * from content where content_key = 'intro_message";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+echo $twig->render('home.twig', ['intro_message'], $row);
+$stmt->close();
