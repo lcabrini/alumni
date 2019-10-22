256c9bb0a8718f0360a6be4b42f049b2033a2c84
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-22 16:36:52 +0000

    Display current content in content form.

--- alumni/routes/edit_content.php
+++ alumni/routes/edit_content.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('util.php');
+require_once('content.php');
 
 if (($_SESSION['user'] != 1)) {
     // TODO: what should really be done here?
@@ -12,8 +13,8 @@ if (($_SESSION['user'] != 1)) {
         $parts = explode("/", $_SERVER['REQUEST_URI']);
         $content_key = $parts[2];
         $twig = get_twig();
-        // TODO: send content to form.
+        $content = get_content_by_key($content_key);
         echo $twig->render('content_edit_form.twig', 
-            ['key' => $content_key]);
+            ['content' => $content]);
     }
 }
