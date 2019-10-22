12085b4bd399f07d282817d5ee8524ede9c0bba6
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-22 16:27:15 +0000

    Implemented CRUD functions for content.

---
+++ alumni/content.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('util.php');
+
+function get_content_by_key($key) {
+    $db = get_database_connection();
+    $sql = "select * from content where content_key = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("s", $key);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    return $row;
+}
+
+function update_content($content) {
+    $db = get_database_connection();
+    $sql = "update content set title = ?, subtitle = ?, body = ? " .
+        "where content_key = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("ssss", $content->title, $content->subtitle,
+        $content->body, $content->content_key);
+    $stmt->execute();
+}
