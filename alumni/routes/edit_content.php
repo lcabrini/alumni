77b39785470edf4e7aad7032c5b7bf1223e6bade
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-22 16:19:36 +0000

    Check if content edit got GET or POST. Refs #12, #13

--- alumni/routes/edit_content.php
+++ alumni/routes/edit_content.php
@@ -6,9 +6,14 @@ if (($_SESSION['user'] != 1)) {
     // TODO: what should really be done here?
     header("Location: /");
 } else {
-    $parts = explode("/", $_SERVER['REQUEST_URI']);
-    $content_key = $parts[2];
-    $twig = get_twig();
-    // TODO: send content to form.
-    echo $twig->render('content_edit_form.twig', ['key' => $content_key]);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo "POST";
+    } else {
+        $parts = explode("/", $_SERVER['REQUEST_URI']);
+        $content_key = $parts[2];
+        $twig = get_twig();
+        // TODO: send content to form.
+        echo $twig->render('content_edit_form.twig', 
+            ['key' => $content_key]);
+    }
 }
