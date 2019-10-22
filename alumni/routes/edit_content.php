10a85ebcf42977a53253ec2216f82f5aafbeb29c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-22 10:38:18 +0000

    The beginnings of a content editing form. Refs #13

---
+++ alumni/routes/edit_content.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('util.php');
+
+if (($_SESSION['user'] != 1)) {
+    // TODO: what should really be done here?
+    header("Location: /");
+} else {
+    $parts = explode("/", $_SERVER['REQUEST_URI']);
+    $content_key = $parts[2];
+    $twig = get_twig();
+    // TODO: send content to form.
+    echo $twig->render('content_edit_form.twig', ['key' => $content_key]);
+}
