61765f26fcee5f80c7316fdf2035ad0ca791c964
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-23 20:48:14 +0000

    Prevent the crontab for being installed multiple times. Closes #20

--- install_crontab.zsh
+++ install_crontab.zsh
@@ -1,4 +1,9 @@
 #! /bin/zsh
 
+if [[ -n $(crontab -l | grep alumni) ]]; then
+    print "Crontab for alumni already installed. Aborting."
+    exit
+fi
+
 script=$0:A:h/cronjob.zsh
 (crontab -l; print "7 * * * * $script") | crontab -
