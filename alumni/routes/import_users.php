3ca4f22e3e4009c46f0461e07637786caf215301
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-10-26 22:59:00 +0000

    Form to get users CVS file. Refs #22

---
+++ alumni/routes/import_users.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('util.php');
+
+if ($_SESSION['user'] != 1) {
+    require '404.php';
+    die();
+} else {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!isset($_FILES['users_file'])) {
+            $twig = get_twig();
+            $twig->render('500.php');
+        } else {
+            require_once('users.php');
+            $users = parse_users_sheet($_FILES['users_file']['tmp_name']);
+            var_dump($users);
+            die()
+        }
+    } else {
+        $twig = get_twig();
+        echo $twig->render('import_users_form.twig');
+    }
+}
