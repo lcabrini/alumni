1c8d740200c11748f29132cfb0df1dcd059f456c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-03 22:27:09 +0000

    Call get_content_by key in home route. Refs #29

--- alumni/routes/home.php
+++ alumni/routes/home.php
@@ -1,13 +1,16 @@
 <?php
 
 require_once('util.php');
+require_once('content.php');
 
 $twig = get_twig();
+/*
 $db = get_database_connection();
 $sql = "select * from content where content_key = 'intro_message'";
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $result = $stmt->get_result();
-$row = $result->fetch_assoc();
-echo $twig->render('home.twig', ['intro' => $row]);
-$stmt->close();
+$row = $result->fetch_assoc();*/
+$content = get_content_by_key('intro_message');
+echo $twig->render('home.twig', ['intro' => $content]);
+//$stmt->close();
