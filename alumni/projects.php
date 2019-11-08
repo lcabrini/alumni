8bbff164e85e9afa922800551041c0365c358633
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-08 18:48:27 +0000

    Implemented add_project function. Refs #18

--- alumni/projects.php
+++ alumni/projects.php
@@ -10,3 +10,14 @@ function get_active_projects() {
     $result = $stmt->get_result();
     return $result;
 }
+
+function add_project($project) {
+    $db = get_database_connection();
+    $sql = "insert into projects(project_name, description, status) " .
+        "values(?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("sss", $project['project_name'], 
+        $project['description'], $project['status']);
+    $stmt->execute();
+    return $db->insert_id;
+}
