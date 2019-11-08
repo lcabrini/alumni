1c1227b7410d8c0f6b500992b19984f5295f58e5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-08 18:49:18 +0000

    Call add project on project form POST. Refs #18

--- alumni/routes/add_project.php
+++ alumni/routes/add_project.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once('util.php');
+require_once('projects.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    add_project($_POST);
     $_SESSION['message'] = array(
         'type' => 'is-success',
         'text' => 'Added project'
