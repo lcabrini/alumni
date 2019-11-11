cc555ace56e84e836fdec1125724df9a818cf22d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-11 15:53:47 +0000

    Handle public/.htaccess missing. Closes #35

--- devserver.zsh
+++ devserver.zsh
@@ -1,6 +1,13 @@
 #! /bin/zsh
 
 ht=public/.htaccess
+if [[ ! -f $ht ]]; then
+    cp $ht.in $ht
+    print "The file public/htaccess was not found. Creating a new one"
+    print "You will need to configure this before proceeding"
+    exit 1
+fi
+
 while read line; do
     var=$(print $line | awk '{print $2}')
     val=$(print $line | awk '{print $3}')
